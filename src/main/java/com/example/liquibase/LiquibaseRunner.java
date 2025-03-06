28197c3a0a99179db899abe3a35aa499eda39cae
Roman Firsov <firsov_r@stdpr.ru>
2025-03-06 18:23:21 +0300

    .

--- src/main/java/com/example/liquibase/LiquibaseRunner.java
+++ src/main/java/com/example/liquibase/LiquibaseRunner.java
@@ -1,52 +1,34 @@
 package com.example.liquibase;
 
+import liquibase.Contexts;
 import liquibase.Liquibase;
-import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
-import liquibase.resource.ClassLoaderResourceAccessor;
 import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class LiquibaseRunner {
 
+    private static final String URL = "jdbc:postgresql://localhost:5432/liquibase_sandbox";
+    private static final String USER = "romanya";
+    private static final String PASSWORD = "romanya";
+    private static final String CHANGELOG_MASTER_XML = "db/changelog/changelog-master.xml";
+
     public static void main(String[] args) throws LiquibaseException, SQLException {
-        String databaseUrl = "jdbc:postgresql://localhost:5432/postgres";
-        String databaseUser = "postgres";
-        String databasePassword = "";
-        String changelogFile = "db/changelog/db.changelog-master.xml";
 
-        try (Connection connection = DriverManager.getConnection(databaseUrl, databaseUser, databasePassword)) {
-            Database database = DatabaseFactory.getInstance()
-                    .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            var database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            var liquibase = new Liquibase(CHANGELOG_MASTER_XML, new ClassLoaderResourceAccessor(), database);
 
-            Liquibase liquibase = new Liquibase(changelogFile, new ClassLoaderResourceAccessor(), database);
+            liquibase.update(new Contexts());
+//            liquibase.rollback(1, "");
 
-
-            StringWriter stringWriter = new StringWriter();
-            PrintWriter printWriter = new PrintWriter(stringWriter);
-
-//            update(liquibase, printWriter);
-
-            int rollbackCount = 1; // это количество изменений, которые нужно откатить.
-            rollback(liquibase, rollbackCount, printWriter);
-
-            System.out.println(stringWriter.toString());
         }
     }
 
-    public static void update(Liquibase liquibase, PrintWriter printWriter) throws LiquibaseException {
-        liquibase.update("", printWriter);  // Выполняет миграции
-        System.out.println("Database migration completed successfully.");
-    }
 
-    public static void rollback(Liquibase liquibase, int rollbackCount, PrintWriter printWriter) throws LiquibaseException {
-        liquibase.rollback(rollbackCount, "", printWriter);  // Откат указанного количества изменений
-        System.out.println("Rolled back " + rollbackCount + " changeset(s).");
-    }
 }
